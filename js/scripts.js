e79fc1e7eab085c86699c2fe36ae4e07783e368b
Brendan Grubb and Dylan Stackhouse <dylan@>
2017-01-10 10:27:25 -0800

    add jquery functionality to change bg and text color on favorite-things.html

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function() {
+  $("#black-button").click(function() {
+    $("body").removeClass();
+    $("body").addClass("bg-black");
+  })
+  $("#white-button").click(function() {
+    $("body").removeClass();
+    $("body").addClass("bg-white");
+  })
+
+})
